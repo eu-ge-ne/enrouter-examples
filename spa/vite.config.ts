70b516569b3e77142acd4b4896f4a5228bc3b646
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-11-12 22:52:49 -0500

    v0.0.24

--- spa/vite.config.ts
+++ spa/vite.config.ts
@@ -5,7 +5,4 @@ import enrouter from "enrouter/vite/plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), enrouter({ path: "src/app" })],
-  optimizeDeps: {
-    exclude: ["virtual:enrouter"],
-  },
 });
